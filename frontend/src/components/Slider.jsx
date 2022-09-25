17a003b97e85f782dff753828f2a5eb658244ae3
Liying <hlyibic@gmail.com>
2022-09-25 13:12:49 -0400

    create homepage slider

---
+++ frontend/src/components/Slider.jsx
@@ -0,0 +1,133 @@
+import { useState } from 'react';
+import styled from 'styled-components';
+import { ArrowLeftOutlined, ArrowRightOutlined } from '@mui/icons-material';
+import { sliderItems } from '../data';
+
+const Container = styled.div`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  position: relative;
+  overflow: hidden;
+`;
+
+const Arrow = styled.div`
+  width: 50px;
+  height: 50px;
+  background-color: #fff7f7;
+  border-radius: 50%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: absolute;
+  // the following lines will center the arrows vertically
+  top: 0;
+  bottom: 0;
+  margin: auto;
+  // if the direction is left, the distance between the arrow and the left boundry of the container (it's positioned against) will be 10, and vice versa
+  left: ${(props) => props.direction === 'left' && '10px'};
+  right: ${(props) => props.direction === 'right' && '10px'};
+  cursor: pointer;
+  opacity: 0.5;
+  z-index: 2;
+`;
+
+const Wrapper = styled.div`
+  height: 100%;
+  display: flex;
+  // we want all properties to transition with ease effect for 1.5s
+  transition: all 1.5s ease;
+  // translateX(0): repositions an element to its original place
+  // translateX(-100vw): moves an element on the x-axis to the left by 100vw
+  transform: translateX(${(props) => props.slideIndex * -100}vw);
+`;
+
+const Slide = styled.div`
+  width: 100vw;
+  height: 80vh;
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  background-color: ${(props) => props.bg};
+`;
+
+const ImgContainer = styled.div`
+  height: 100%;
+  flex: 1;
+`;
+
+const Image = styled.img`
+  height: 100%;
+  flex: 1;
+`;
+
+const InfoContainer = styled.div`
+  flex: 1;
+  padding: 50px;
+`;
+
+const Title = styled.h1`
+  font-size: 70px;
+`;
+
+const Description = styled.p`
+  margin: 50px 0;
+  font-size: 20px;
+  font-weight: 500;
+  letter-spacing: 3px;
+`;
+
+const Button = styled.button`
+  padding: 10px;
+  font-size: 20px;
+  background-color: transparent;
+  cursor: pointer;
+  border-style: solid;
+`;
+
+const Slider = () => {
+  const [slideIndex, setSlideIndex] = useState(0);
+
+  const handleClick = (direction) => {
+    // 1st image (index 0), 2nd image (index 1), 3rd image (index 2)
+    if (direction === 'left') {
+      // when navigating the left arrow, if the current image is the 2nd/3rd (idx 1/2) one, transition back to the one before (-1), otherwise (on the 1st image) rotate to the 3rd (idx 2) image
+      setSlideIndex(slideIndex > 0 ? slideIndex - 1 : 2);
+    } else {
+      // when navigating the right arrow, if the current image is the 1st/2nd (idx 0/1) one, transition back to the next one in line (+1), otherwise (on the 3rd image) rotate back to the 1st (idx 0) image
+      setSlideIndex(slideIndex < 2 ? slideIndex + 1 : 0);
+    }
+  };
+  return (
+    <div>
+      <Container>
+        <Arrow direction="left" onClick={() => handleClick('left')}>
+          <ArrowLeftOutlined />
+        </Arrow>
+        <Wrapper slideIndex={slideIndex}>
+          {sliderItems.map((item) => {
+            return (
+              <div key={item.id}>
+                <Slide bg={item.bg}>
+                  <ImgContainer>
+                    <Image src={item.img} />
+                  </ImgContainer>
+                  <InfoContainer>
+                    <Title>{item.title}</Title>
+                    <Description>{item.description}</Description>
+                    <Button>SHOP NOW</Button>
+                  </InfoContainer>
+                </Slide>
+              </div>
+            );
+          })}
+        </Wrapper>
+        <Arrow direction="right" onClick={() => handleClick('right')}>
+          <ArrowRightOutlined />
+        </Arrow>
+      </Container>
+    </div>
+  );
+};
+
+export default Slider;
---
+++ frontend/src/data.js
@@ -0,0 +1,25 @@
+export const sliderItems = [
+  {
+    id: 1,
+    // img: 'https://images.pexels.com/photos/5662678/pexels-photo-5662678.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+    img: 'https://cdn.shopify.com/s/files/1/1132/5272/products/editorial_58eaf4c8-331f-4c5c-88bb-aa609b4ba74a_2048x2048.progressive.jpg?v=1655072376',
+    title: 'SUMMER SALE',
+    description: 'FINAL ESSENTIALS SALE - UP TO 70% OFF',
+    bg: 'aliceblue',
+  },
+  {
+    id: 2,
+    img: 'https://cdna.lystit.com/photos/asos/6703f415/asos-Multi-High-Neck-Maxi-Dress-With-Wild-Flower-Embroidery.jpeg',
+    title: 'JUST LAUNCHED',
+    description: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS",
+    bg: 'honeydew',
+  },
+  {
+    id: 3,
+    // img1: 'https://images.pexels.com/photos/8029007/pexels-photo-8029007.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+    img: 'https://specials-images.forbesimg.com/imageserve/6259fd9ee852b389e34f6c79/Best-summer-dresses/960x0.jpg?cropX1=0&cropX2=1000&cropY1=128&cropY2=1127',
+    title: "DESIGNERS' PICKS",
+    description: 'INSPIRED BY ALL - EMPOWERING ALL',
+    bg: 'oldlace',
+  },
+];
