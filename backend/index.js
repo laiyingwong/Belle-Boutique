11e419bbe866660d3065059e7bc6ca5af7493217
Liying <hlyibic@gmail.com>
2022-10-13 12:20:00 -0400

    implement CRUD functionality for user, product, cart and order routes

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const userRoute = require('./routes/user');
 const authRoute = require('./routes/auth');
+const productRoute = require('./routes/products');
 
 dotenv.config();
 
@@ -14,11 +15,12 @@ mongoose
     console.log(err);
   });
 
-// parses incoming JSON requests and puts the parsed data in req
+// allows JSON data in a request and parses incoming JSON requests and puts the parsed data in req
 app.use(express.json());
 
 app.use('/api/auth', authRoute);
 app.use('/api/users', userRoute);
+app.use('/api/products', productRoute);
 
 app.listen(process.env.PORT || 4000, () => {
   console.log('Backend server is running!');
