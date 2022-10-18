5b9cd4157fb44e1f6d939093906711f3d83f892f
Liying <hlyibic@gmail.com>
2022-10-18 16:18:11 -0400

    add homepage demo ima gif

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,9 @@ const dotenv = require('dotenv');
 const userRoute = require('./routes/user');
 const authRoute = require('./routes/auth');
 const productRoute = require('./routes/product');
+const cartRoute = require('./routes/cart');
+const orderRoute = require('./routes/order');
+const stripeRoute = require('./routes/stripe');
 
 dotenv.config();
 
@@ -21,6 +24,9 @@ app.use(express.json());
 app.use('/api/auth', authRoute);
 app.use('/api/users', userRoute);
 app.use('/api/products', productRoute);
+app.use('/api/carts', cartRoute);
+app.use('/api/orders', orderRoute);
+app.use('/api/checkout', stripeRoute);
 
 app.listen(process.env.PORT || 5050, () => {
   console.log('Backend server is running!');
